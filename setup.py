488e72ba1b4e00b857e9a7df13ca2fd8c9b93e07
Omar Khan <omar@omarkhan.me>
2014-03-06 22:05:03 +0700

    Include README in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
+with open('README.rst') as readme:
+    long_description = readme.read()
+
 setup(
     name='flickrstock',
     version='0.1',
     description='Download stock photos from flickr',
+    long_description=long_description,
     author='Omar Khan',
     author_email='omar@omarkhan.me',
     url='https://github.com/omarkhan/flickrstock',
